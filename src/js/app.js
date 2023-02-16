fc278e42cdca19bc142c4da23c9c2c28ffab1a5c
EffySolorzano <solorzano.steph@gmail.com>
2023-02-16 02:26:49 +0000

    firstcommit

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,23 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name ? variables.name : ""}</h1>
+          <h1>${variables.lastname ? variables.lastname : ""}</h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <ul class="${variables.socialMediaPosition}">
+          <li><a href="https://twitter.com/${
+            variables.twitter ? variables.twitter : "4geeksacademy"
+          }"<i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "4geeksacademy"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin ? variables.linkedin : "4geeksacademy"
+            }><i class="fa-brands fa-linkedin-in"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : "4geeksacademy"
+            }><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +63,10 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background: "https://i.ytimg.com/vi/tvsqGkk3F3U/maxresdefault.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://lh3.googleusercontent.com/a/AEdFTp6ez9ohERFZWfzU4XENug0FlMG3W9yv2p9mtEpIBx4=s96-c-rg-br100",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
